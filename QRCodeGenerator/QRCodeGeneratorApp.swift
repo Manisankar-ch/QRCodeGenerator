1a18ad5173c4784ffeb9b1568c84b487a76b9b4b
Mani sankar <msshankar.ch@gmail.com>
2024-11-05 21:48:37 +0530

    Initial Commit

---
+++ QRCodeGenerator/QRCodeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeGeneratorApp.swift
+//  QRCodeGenerator
+//
+//  Created by Mani Sankar on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
